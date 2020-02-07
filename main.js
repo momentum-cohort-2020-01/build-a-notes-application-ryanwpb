9af20ff129d71e4ea18a06bda3050b8c361ee5ce
Ryan Graham <ryangrahamwpb@gmail.com>
2020-02-07 09:13:55 -0500

    Testing posting a new note.

--- main.js
+++ main.js
@@ -20,10 +20,38 @@ function createNotesHTML(notes) {
   return notessStr;
 }
 
-function renderNoteList(notes) {
-  const notesHTML = createNotesHTML(notes);
-  const notesSection = document.querySelector("#notes");
-  notesSection.innerHTML = notesHTML;
+function createNoteHTML(note) {
+  console.log(note);
+  return `<li data-todo-id="${note.id}">${note.note}</li>`;
+}
+
+function renderNoteList(note) {
+  const noteHTML = createNoteHTML(note);
+  const noteList = document.querySelector("#note-list");
+  noteList.insertAdjacentHTML("beforeend", noteHTML);
 }
 
 getAllNotes().then(renderNoteList);
+
+let noteSubmit = document.querySelector("#new-note-form");
+noteSubmit.addEventListener("submit", event => {
+  event.preventDefault();
+  console.log(event);
+  const noteTextField = document.querySelector("#note-text");
+  const noteText = noteTextField.value;
+  noteTextField.value = "";
+  postNewNote(noteText).then(renderNewNote);
+});
+
+function postNewNote(noteText) {
+  return fetch("http://localhost:3000/notes/", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      note: id,
+      note: noteText,
+      done: false,
+      created: moment().format()
+    })
+  }).then(response => response.json());
+}
